86b78bbb0afb87cae5a3dcab9d13846fff5e5047
Hoang-Nam2711 <hoang.nam271199@gmail.com>
2025-02-11 00:34:33 +0900

    Add index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CardPage from './route/CardPage';
+import HomePage from './route/homePage';
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/righteous"
+import '@fontsource/zen-old-mincho';
+import '@fontsource/playfair-display'
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/card" element={<CardPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
